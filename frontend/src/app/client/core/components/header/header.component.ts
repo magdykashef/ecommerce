74923324e88388e250a5150c1c93f426e3c47073
wezaa <wezaa.mahmoud@gmail.com>
2022-06-07 18:00:00 +0200

    #1 update header

--- frontend/src/app/client/core/components/header/header.component.ts
+++ frontend/src/app/client/core/components/header/header.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { AuthService } from "./../../../../security/auth.service";
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-header',
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  selector: "app-header",
+  templateUrl: "./header.component.html",
+  styleUrls: ["./header.component.scss"],
 })
-export class HeaderComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+export class HeaderComponent {
+  title = "test";
+  dropdownOpen: boolean = false;
+  dropdownNavbar: boolean = false;
+  constructor(public authService: AuthService) {}
+  Search(search: string) {
+    console.log(search);
   }
-
 }
