062401ff5b926f79d8a6b3a64675597086600642
wezaa <wezaa.mahmoud@gmail.com>
2022-06-08 01:22:33 +0200

    #4 update header with fix issues and redesign

--- frontend/src/app/client/core/components/header/header.component.ts
+++ frontend/src/app/client/core/components/header/header.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { AuthService } from './../../../../security/auth.service';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+export class HeaderComponent {
+  title = 'Ecommerce';
+  dropdownOpen: boolean = false;
+  dropdownNavbar: boolean = false;
+  constructor(public authService: AuthService) {}
+  searchProducts(search: string) {
+    console.log(search);
   }
-
 }
