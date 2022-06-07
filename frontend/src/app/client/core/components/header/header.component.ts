c109b80abec2c2cfe8f1dc71434b9db2b437bfb9
wezaa <wezaa.mahmoud@gmail.com>
2022-06-07 19:24:45 +0200

    #2 update header with fix issues

--- frontend/src/app/client/core/components/header/header.component.ts
+++ frontend/src/app/client/core/components/header/header.component.ts
@@ -1,17 +1,17 @@
-import { AuthService } from "./../../../../security/auth.service";
-import { Component } from "@angular/core";
+import { AuthService } from './../../../../security/auth.service';
+import { Component } from '@angular/core';
 
 @Component({
-  selector: "app-header",
-  templateUrl: "./header.component.html",
-  styleUrls: ["./header.component.scss"],
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent {
-  title = "test";
+  title = 'Ecommerce';
   dropdownOpen: boolean = false;
   dropdownNavbar: boolean = false;
   constructor(public authService: AuthService) {}
-  Search(search: string) {
+  searchProducts(search: string) {
     console.log(search);
   }
 }
