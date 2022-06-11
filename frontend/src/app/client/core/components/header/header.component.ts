a4a5e8c3b89d4f32fa82ab5494d1b365e9a1e086
Magdy <magdy@Magdy-MacBook-Pro.local>
2022-06-11 15:01:25 +0200

    fix: show/hide user_name after login/logout

--- frontend/src/app/client/core/components/header/header.component.ts
+++ frontend/src/app/client/core/components/header/header.component.ts
@@ -11,7 +11,21 @@ export class HeaderComponent {
   brand = 'assets/pngwing.com.png';
   dropdownOpen = false;
   dropdownNavbar = false;
-  constructor(public authService: AuthService) {}
+  user_name = '';
+
+  constructor(public authService: AuthService) { }
+
+  ngOnInit(): void {
+    this.authService.user$.subscribe(
+      (user) => {
+        if (user) this.user_name = user.user_name;
+        else this.user_name = '';
+      },
+      () => this.user_name = ''
+    );
+  }
+
+
   searchProducts(search: string) {
     console.log(search);
   }
