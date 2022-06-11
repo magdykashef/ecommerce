395152ba4da067ef77d46e8f4195110191e4fb23
Magdy <magdy@Magdy-MacBook-Pro.local>
2022-06-11 16:25:37 +0200

    Fix: save user data in localStorage and userSubject

--- frontend/src/app/shared/services/user.service.ts
+++ frontend/src/app/shared/services/user.service.ts
@@ -1,15 +1,20 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, tap } from 'rxjs';
 import { environment } from './../../../environments/environment';
 import { User } from './../models/user';
+import { AuthService } from '../../security/auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor(private httpClient: HttpClient) { }
+  constructor(
+    private httpClient: HttpClient,
+    private authService: AuthService,
+
+  ) { }
 
 
   getAllUsers(): Observable<User[]> {
@@ -23,15 +28,14 @@ export class UserService {
 
 
   createUser(user: User): Observable<User> {
-    return this.httpClient.post<User>(`${environment.api}/user/create`, {
-      user_name: user.user_name,
-      first_name: user.first_name,
-      last_name: user.last_name,
-      email: user.email,
-      password: user.password,
-      address: user.address,
-      phone: user.phone
-    });
+    localStorage.removeItem('user');
+    this.authService.userSubject.next(null);
+
+    return this.httpClient.post<User>(`${environment.api}/user/create`, user)
+      .pipe(tap((user) => {
+        localStorage.setItem('user', JSON.stringify(user));
+        this.authService.userSubject.next(user);
+      }));
   }
 
 
