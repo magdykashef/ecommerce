8e23e203efc2c3d8aaf953260ff8414032c8ec02
Magdy <magdy@Magdy-MacBook-Pro.local>
2022-06-12 15:19:06 +0200

    Feat: add product class model

--- frontend/src/app/shared/models/product.ts
+++ frontend/src/app/shared/models/product.ts
@@ -1,12 +1,48 @@
 export interface Product {
-    id?: string;
-    code?: string;
-    name?: string;
-    description?: string;
-    price?: number;
-    quantity?: number;
-    inventoryStatus?: string;
-    category?: string;
-    image?: string;
-    rating?: number;
+	id?: string;
+	code?: string;
+	name?: string;
+	description?: string;
+	price?: number;
+	quantity?: number;
+	inventoryStatus?: string;
+	category?: string;
+	image?: string;
+	rating?: number;
+}
+
+export interface Product {
+	id?: string;
+	code?: string;
+	name?: string;
+	description?: string;
+	price?: number;
+	quantity?: number;
+	inventoryStatus?: string;
+	category?: string;
+	image?: string;
+	rating?: number;
+}
+
+export class Product {
+	id?: string;
+	name?: string;
+	description?: string;
+	price?: number;
+	discount?: number;
+	quantity?: number;
+	category_id?: string;
+	category_name?: string;
+	images?: string[];
+	image1: string;
+	image2: string;
+	image3: string;
+	image4: string;
+	image5: string;
+	reviews?: [{
+		user_id: string;
+		user_name: string;
+		rating: number;
+		comment: string;
+	}];
 }
