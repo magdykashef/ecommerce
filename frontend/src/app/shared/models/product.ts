e0ac23ad3f3a358dbd4290109e8a44e4c0d743ad
Magdy <magdy@Magdy-MacBook-Pro.local>
2022-06-12 22:03:07 +0200

    update product class model

--- frontend/src/app/shared/models/product.ts
+++ frontend/src/app/shared/models/product.ts
@@ -11,19 +11,6 @@ export interface Product {
 	rating?: number;
 }
 
-export interface Product {
-	id?: string;
-	code?: string;
-	name?: string;
-	description?: string;
-	price?: number;
-	quantity?: number;
-	inventoryStatus?: string;
-	category?: string;
-	image?: string;
-	rating?: number;
-}
-
 export class Product {
 	id?: string;
 	name?: string;
@@ -34,11 +21,11 @@ export class Product {
 	category_id?: string;
 	category_name?: string;
 	images?: string[];
-	image1: string;
-	image2: string;
-	image3: string;
-	image4: string;
-	image5: string;
+	image1?: string;
+	image2?: string;
+	image3?: string;
+	image4?: string;
+	image5?: string;
 	reviews?: [{
 		user_id: string;
 		user_name: string;
