fb5d07065949bf6c81636685c1558357e6aef7dc
Magdy <magdy@Magdy-MacBook-Pro.local>
2022-06-12 23:14:12 +0200

    Fix: display user_name in admin header

--- frontend/src/app/admin/core/components/top-bar/app.topbar.component.ts
+++ frontend/src/app/admin/core/components/top-bar/app.topbar.component.ts
@@ -1,18 +1,30 @@
 import { Component, OnInit } from '@angular/core';
 import { AdminComponent } from '../../../admin.component';
 import { MenuItem } from 'primeng/api';
+import { AuthService } from '../../../../security/auth.service';
 
 @Component({
-    selector: 'app-topbar',
-    templateUrl: './app.topbar.component.html'
+	selector: 'app-topbar',
+	templateUrl: './app.topbar.component.html'
 })
 export class AppTopBarComponent implements OnInit {
 
-    items: MenuItem[];
-    user_name = '';
+	items: MenuItem[];
+	user_name = '';
 
-    constructor(public admin: AdminComponent) { }
-    ngOnInit(): void {
-        this.user_name = localStorage.getItem('user_name') || '';
-    }
+	constructor(
+		public admin: AdminComponent,
+		public authService: AuthService,
+	) { }
+
+
+	ngOnInit(): void {
+		this.authService.user$.subscribe(
+			(user) => {
+				if (user) this.user_name = user.user_name;
+				else this.user_name = '';
+			},
+			() => this.user_name = ''
+		);
+	}
 }
