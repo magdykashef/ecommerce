f9c89e54440267d60c4c4003355102371216bc68
Magdy <magdy@Magdy-MacBook-Pro.local>
2022-06-12 07:08:08 +0200

    Fix: small enhancements

--- frontend/src/app/client/pages/contact-us/contact-us.component.ts
+++ frontend/src/app/client/pages/contact-us/contact-us.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Subscription } from 'rxjs';
-import { ContactUsService } from 'src/app/shared/services/contact-us.service';
+import { ContactUsService } from '../../../shared/services/contact-us.service';
 
 @Component({
   selector: 'app-contact-us',
@@ -10,72 +10,68 @@ import { ContactUsService } from 'src/app/shared/services/contact-us.service';
 })
 export class ContactUsComponent implements OnInit, OnDestroy {
 
-    contactUsForm!: FormGroup;
-    statusMessage: string= '';
-    submitted: boolean = false;
-    subscriptions: Subscription[] = [];
+  contactUsForm!: FormGroup;
+  statusMessage: string = '';
+  submitted: boolean = false;
+  subscriptions: Subscription[] = [];
 
   constructor(
     private formBuilder: FormBuilder,
     private ContactUsService: ContactUsService
   ) { }
 
-    ngOnInit(): void {
-        this.contactUsForm = this.formBuilder.group({
-            email: ["", [Validators.required, Validators.email]],
-            message: [
-                "",
-                [
-                    Validators.required,
-                    Validators.minLength(50),
-                    Validators.maxLength(500),
-                ]
-            ],
-            phone: [
-              "",
-              [Validators.required, Validators.pattern("^((\\+91-?)|0)?[0-9]{10}$")],
-            ],
-          });
-        }
-      
-        ngOnDestroy(): void {
-          for (const subscription of this.subscriptions) {
-            subscription.unsubscribe();
+  ngOnInit(): void {
+    this.contactUsForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      message: [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(50),
+          Validators.maxLength(1000),
+        ]
+      ],
+      phone: [
+        '',
+        [Validators.required, Validators.pattern('^((\\+91-?)|0)?[0-9]{10}$')],
+      ],
+    });
+  }
+
+  ngOnDestroy(): void {
+    for (const subscription of this.subscriptions) {
+      subscription.unsubscribe();
+    }
+  }
+
+  onSubmit() {
+    this.submitted = true;
+    this.subscriptions.push(
+      this.ContactUsService
+        .sendContactUs(this.contactUsForm.value)
+        .subscribe(
+          (res: { [key: string]: string }) => {
+            this.contactUsForm.reset();
+            this.statusMessage = res.statusMessage;
+          },
+          (res: { [key: string]: string }) => {
+            this.statusMessage = res.statusMessage;
+            this.submitted = false;
           }
-        }
-      
-        onSubmit() {
-          this.submitted = true;
-          this.subscriptions.push(
-            this.ContactUsService
-              .sendContactUs({
-                email: this.contactUsForm.value.email,
-                message: this.contactUsForm.value.message,
-                phone: this.contactUsForm.value.phone,
-              })
-              .subscribe(
-                (res: { [key: string]: string }) => {
-                  this.contactUsForm.reset();
-                  this.statusMessage = res.statusMessage;
-                },
-                (res: { [key: string]: string }) => {
-                  this.statusMessage = res.statusMessage;
-                  this.submitted = false;
-                }
-              )
-          );
-        }
-      
-        get email() {
-          return this.contactUsForm.get("email");
-        }
-      
-        get message() {
-          return this.contactUsForm.get("message");
-        }
-      
-        get phone() {
-          return this.contactUsForm.get("phone");
+        )
+    );
+  }
+
+  get email() {
+    return this.contactUsForm.get('email');
+  }
+
+  get message() {
+    return this.contactUsForm.get('message');
+  }
+
+  get phone() {
+    return this.contactUsForm.get('phone');
   }
 
 }
